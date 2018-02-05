f17496e86eb31ac9c86fcdd0408c7a62ddca5b9a
dolgarev <o.dolgarev@gmail.com>
2018-02-05 09:47:34 +0200

    some improvements

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
+/* global Package */
+
 Package.describe({
   name: 'liberation:ddp-rate-limiter-mixin',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This is a mixin for mdg:validated-method to add rate limitation support to Meteor\'s methods.',
   // URL to the Git repository containing the source code for this package.
@@ -8,17 +10,17 @@ Package.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.5.2');
-  api.use(['ecmascript', 'ddp-rate-limiter', 'underscore'], 'server');
-  api.mainModule('ddp-rate-limiter-mixin.js', 'server');
-});
+Package.onUse(function (api) {
+  api.versionsFrom('1.5.2')
+  api.use(['ecmascript', 'ddp-rate-limiter', 'underscore'], 'server')
+  api.mainModule('ddp-rate-limiter-mixin.js', 'server')
+})
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('liberation:ddp-rate-limiter-mixin');
-  api.mainModule('ddp-rate-limiter-mixin-tests.js');
-});
+Package.onTest(function (api) {
+  api.use('ecmascript')
+  api.use(['tinytest', 'ddp-rate-limiter', 'underscore'], 'server')
+  api.use('liberation:ddp-rate-limiter-mixin', 'server')
+  api.mainModule('ddp-rate-limiter-mixin-tests.js')
+})
